6a170425b14f63389fdc17b5502eb06998bf80de
Abdurauf <you@example.com>
2025-10-21 17:25:56 +0500

    added for Abdurauf

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../../App.css"
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className=' header w-full h-[70px] '>
+      <ul className='flex gap-6'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/team">Find a Team</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/articles">Articles</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
+        <li><Link to="/contact">Contact Us</Link></li>
+      </ul>
     </div>
   )
 }
